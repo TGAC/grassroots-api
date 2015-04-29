b09e5fdb240dd15171a17ca2dbf14db93b1de188
billyfish <tyrrell.simon@gmail.com>
2015-04-29 11:36:54 +0100

    wip

--- services/test_long_runner/src/long_running_service.c
+++ services/test_long_runner/src/long_running_service.c
@@ -9,6 +9,7 @@
 static uint32 S_DEFAULT_DURATION = 30;
 
 
+
 typedef struct
 {
 	time_t tt_start;
@@ -22,8 +23,8 @@ typedef struct
 typedef struct 
 {
 	ServiceData lsd_base_data;
-	TimedTask lsd_task_0;
-	TimedTask lsd_task_1;
+	TimedTask *lsd_tasks_p;
+	uint32 lsd_num_tasks;
 } LongRunningServiceData;
 
 
@@ -125,36 +126,58 @@ static LongRunningServiceData *AllocateLongRunningServiceData (Service *service_
 {
 	LongRunningServiceData *data_p = (LongRunningServiceData *) AllocMemory (sizeof (LongRunningServiceData));
 
-	InitTimedTask (& (data_p -> lsd_task_0));
-	InitTimedTask (& (data_p -> lsd_task_1));
+	if (data_p)
+		{
+			data_p -> lsd_num_tasks = 3;
 
-	return data_p;
+			if ((data_p -> lsd_tasks_p = (TimedTask *) AllocMemory (sizeof (TimedTask) * (data_p -> lsd_num_tasks))) != NULL)
+				{
+					uint32 i;
+
+					for (i = 0; i < data_p -> lsd_num_tasks; ++ i)
+						{
+							InitTimedTask ((data_p -> lsd_tasks_p) + i);
+						}
+
+					return data_p;
+				}
+
+			FreeMemory (data_p);
+		}
+
+	return NULL;
 }
 
 
 static void FreeLongRunningServiceData (LongRunningServiceData *data_p)
 {
+	FreeMemory (data_p -> lsd_tasks_p);
 	FreeMemory (data_p);
 }
 
  
 static bool CloseLongRunningService (Service *service_p)
 {
-	bool close_flag = false;
+	bool close_flag = true;
 	LongRunningServiceData *data_p = (LongRunningServiceData *) (service_p -> se_data_p);
-	OperationStatus status = GetCurrentTimedTaskStatus (& (data_p -> lsd_task_0));
+	TimedTask *t_p = data_p -> lsd_tasks_p;
+	uint32 i;
 
-	if (status == OS_SUCCEEDED || status == OS_IDLE || status == OS_FAILED)
+	for (i = 0; i < data_p -> lsd_num_tasks; ++ i, ++ t_p)
 		{
-			OperationStatus status = GetCurrentTimedTaskStatus (& (data_p -> lsd_task_1));
+			OperationStatus status = GetCurrentTimedTaskStatus (t_p);
 
-			if (status == OS_SUCCEEDED || status == OS_IDLE || status == OS_FAILED)
+			if (status == OS_PENDING || status == OS_STARTED)
 				{
-					FreeLongRunningServiceData (data_p);
-					FreeService (service_p);
-
-					close_flag = true;
+					close_flag = false;
+					i = data_p -> lsd_num_tasks; 		/* force exit from loop */
 				}
+		}
+
+	if (close_flag)
+		{
+			FreeLongRunningServiceData (data_p);
+			FreeService (service_p);
 		}
 
 	return close_flag;
@@ -206,16 +229,17 @@ static json_t *GetLongRunningResultsAsJSON (Service *service_p, const uuid_t job
 {
 	LongRunningServiceData *data_p = (LongRunningServiceData *) (service_p -> se_data_p);
 	json_t *resource_json_p = NULL;
-
 	TimedTask *task_p = NULL;
+	TimedTask *t_p = data_p -> lsd_tasks_p;
+	uint32 i;
 
-	if (uuid_compare (data_p -> lsd_task_0.tt_job_p -> sj_id, job_id) == 0)
+	for (i = 0; i < data_p -> lsd_num_tasks; ++ i, ++ t_p)
 		{
-			task_p = & (data_p -> lsd_task_0);
-		}
-	else if (uuid_compare (data_p -> lsd_task_1.tt_job_p -> sj_id, job_id) == 0)
-		{
-			task_p = & (data_p -> lsd_task_1);
+			if (uuid_compare (t_p -> tt_job_p -> sj_id, job_id) == 0)
+				{
+					task_p = t_p;
+					i = data_p -> lsd_num_tasks; 		/* force exit from loop */
+				}
 		}
 
 	if (task_p)
@@ -232,37 +256,24 @@ static json_t *GetLongRunningResultsAsJSON (Service *service_p, const uuid_t job
 static ServiceJobSet *RunLongRunningService (Service *service_p, ParameterSet *param_set_p, json_t *credentials_p)
 {
 	LongRunningServiceData *data_p = (LongRunningServiceData *) (service_p -> se_data_p);
-	const size_t NUM_TASKS = 2;
-
-	service_p -> se_jobs_p = AllocateServiceJobSet (service_p, NUM_TASKS);
+	service_p -> se_jobs_p = AllocateServiceJobSet (service_p, data_p -> lsd_num_tasks);
 
 	if (service_p -> se_jobs_p)
 		{
 			ServiceJob *job_p = service_p -> se_jobs_p -> sjs_jobs_p;
+			TimedTask *task_p = data_p -> lsd_tasks_p;
+			uint32 i;
 
-			/* simulate a task that takes 15 seconds */
-			data_p -> lsd_task_0.tt_job_p = job_p;
-			StartTimedTask (& (data_p -> lsd_task_0), 15);
-
-			job_p -> sj_status = GetCurrentTimedTaskStatus (& (data_p -> lsd_task_0));
-			/*
-			if (job_p -> sj_status == OS_STARTED)
+			for (i = 0; i < data_p -> lsd_num_tasks; ++ i, ++ job_p, ++ task_p)
 				{
-					AddServiceJobToStatusTable (job_p -> sj_id, job_p);
-				}
-			*/
-			++ job_p;
+					/* get a randomish duration between 0 and 120 seconds */
+					size_t duration = rand () % 120;
 
-			/* simulate a task that takes 3 minutes */
-			data_p -> lsd_task_1.tt_job_p = job_p;
-			StartTimedTask (& (data_p -> lsd_task_1), 180);
-			job_p -> sj_status = GetCurrentTimedTaskStatus (& (data_p -> lsd_task_1));
-			/*
-			if (job_p -> sj_status == OS_STARTED)
-				{
-					AddServiceJobToStatusTable (job_p -> sj_id, job_p);
+					task_p -> tt_job_p = job_p;
+					StartTimedTask (task_p, duration);
+					job_p -> sj_status = GetCurrentTimedTaskStatus (task_p);
 				}
-			*/
+
 		}		/* if (service_p -> se_jobs_p) */
 
 	return service_p -> se_jobs_p;
@@ -286,13 +297,16 @@ static OperationStatus GetLongRunningServiceStatus (Service *service_p, const uu
 
 			if (job_p)
 				{
-					if (data_p -> lsd_task_0.tt_job_p == job_p)
+					TimedTask *task_p = data_p -> lsd_tasks_p;
+					uint32 i;
+
+					for (i = 0; i < data_p -> lsd_num_tasks; ++ i, ++ task_p)
 						{
-							status = GetCurrentTimedTaskStatus (& (data_p -> lsd_task_0));
-						}
-					else if (data_p -> lsd_task_0.tt_job_p == job_p)
-						{
-							status = GetCurrentTimedTaskStatus (& (data_p -> lsd_task_0));
+							if (task_p -> tt_job_p == job_p)
+								{
+									status = GetCurrentTimedTaskStatus (task_p);
+									i = data_p -> lsd_num_tasks; 		/* force exit from loop */
+								}
 						}
 
 					job_p -> sj_status = status;
