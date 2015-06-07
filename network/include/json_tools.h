3a26690974e508bf46d69c3fc6fc4eb8d9bb7b70
Simon Tyrrell <tyrrell.simon@gmail.com>
2015-06-07 18:01:35 +0100

    more docs

--- network/include/json_tools.h
+++ network/include/json_tools.h
@@ -17,8 +17,14 @@ extern "C"
 {
 #endif
 
+/**
+ *
+ */
 WHEATIS_NETWORK_API int SendJsonRawRequest (const json_t *json_p, RawConnection *connection_p);
 
+/**
+ *
+ */
 WHEATIS_NETWORK_API json_t *GetLoginJsonRequest (const char * const username_s, const char *password_s);
 
 WHEATIS_NETWORK_API json_t *GetModifiedFilesRequest (const char * const username_s, const char * const password_s, const char * const from_s, const char * const to_s);
@@ -33,26 +39,72 @@ WHEATIS_NETWORK_API json_t *GetKeywordServicesRequest (const char * const userna
 
 WHEATIS_NETWORK_API json_t *GetNamedServicesRequest (const char * const username_s, const char * const password_s, const char * const service_name_s);
 
+
+/**
+ * Generate the JSON fragment to send to the Server for checking the status of an Operation.
+ *
+ * @param username_s An optional username.
+ * @param password_s An optional encrypted password.
+ * @param service_uuid_s The uuid for the requested Operation.
+ * @return The JSON fragment to send to the Server or <code>
+ * NULL</code> upon error.
+ */
 WHEATIS_NETWORK_API json_t *GetCheckServicesRequest (const char * const username_s, const char * const password_s, const char * const service_uuid_s);
 
 WHEATIS_NETWORK_API bool AddCredentialsToJson (json_t *root_p, const char * const username_s, const char * const password_s);
 
+/**
+ * Get the JSON fragment for a given Operation.
+ *
+ * @param op The Operation to generate the JSON for.
+ * @return The JSON fragment or <code>NULL</code> upon error.
+ */
 WHEATIS_NETWORK_API json_t *GetOperationAsJSON (Operation op);
 
 WHEATIS_NETWORK_API json_t *GetServicesRequest (const char * const username_s, const char * const password_s, const Operation op, const char * const op_key_s, json_t * const op_data_p);
 
+/**
+ * Send a JSON-based request to the server and get the JSON-based response.
+ *
+ * @param req_p The JSON request to send to the Server.
+ * @param connection_p The Connection to the Server.
+ * @return The JSON response or <code>NULL</code> upon error.
+ */
 WHEATIS_NETWORK_API json_t *MakeRemoteJsonCall (json_t *req_p, Connection *connection_p);
 
+
+/**
+ * Delete a json object and all of its references to its children
+ *
+ * @param json_ The json object to remove.
+ */
 WHEATIS_NETWORK_API void WipeJSON (json_t *json_p);
 
 WHEATIS_NETWORK_API const char *GetUserUUIDStringFromJSON (const json_t *credentials_p);
 
 
+/**
+ * Generate the JSON fragment to send to the Server for checking the status of one or more Operations.
+ *
+ * @param ids_pp An array of uuid_ts, one for each operation .
+ * @param num_ids The number of ids in ids_pp.
+ * @param connection_p The Connection to the Server.
+ * @return The JSON fragment to send to the Server or <code>
+ * NULL</code> upon error.
+ */
 WHEATIS_NETWORK_API json_t *GetServicesStatusRequest (const uuid_t **ids_pp, const uint32 num_ids, Connection *connection_p);
 
 WHEATIS_NETWORK_API json_t *GetServicesResultsRequest (const uuid_t **ids_pp, const uint32 num_ids, Connection *connection_p);
 
 
+/**
+ * Get the current OperationStatus for a Service from its JSON fragment.
+ *
+ * @param service_json_p The JSON fragment of the response from the Server.
+ * @param status_p Pointer to the OperationStatus value that will be updated.
+ * @return <code>true</code> if the OperationStatus value was updated successfully, <code>
+ * false</code> otherwise.
+ */
 WHEATIS_NETWORK_API bool GetStatusFromJSON (const json_t *service_json_p, OperationStatus *status_p);
 
 
--- network/include/key_value_pair.h
+++ network/include/key_value_pair.h
@@ -10,9 +10,15 @@
 
 #include "network_library.h"
 
+/**
+ * A pair of strings.
+ */
 typedef struct KeyValuePair
 {
+	/** The string used as a key */
 	char *kvp_key_s;
+
+	/** The string used as a value */
 	char *kvp_value_s;
 } KeyValuePair;
 
@@ -22,9 +28,25 @@ extern "C"
 {
 #endif
 
+/**
+ * Allocate a KeyValuePair.
+ *
+ * @param key_s This string will be deep copied and be the kvp_key_s in the KeyValuePair.
+ * @param key_s This string will be deep copied and be the kvp_value_s in the KeyValuePair.
+ * @return A newly-allocated KeyValuePair or <code>NULL</code> upon error.
+ * @memberof KeyValuePair
+ */
 WHEATIS_NETWORK_API KeyValuePair *AllocateKeyValuePair (const char *key_s, const char *value_s);
 
+
+/**
+ * Free a KeyValuePair.
+ *
+ * @param kvp_pThe KeyValuePair to free.
+ * @memberof KeyValuePair
+ */
 WHEATIS_NETWORK_API void FreeKeyValuePair (KeyValuePair *kvp_p);
+
 
 #ifdef __cplusplus
 }
