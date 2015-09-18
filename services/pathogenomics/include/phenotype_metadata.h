954724c118721974ae44cbcd4a83d3d5a11fe33d
billyfish <tyrrell.simon@gmail.com>
2015-09-18 16:58:59 +0100

    wip

---
+++ services/pathogenomics/include/phenotype_metadata.h
@@ -0,0 +1,30 @@
+/*
+ * phenotype_metadata.h
+ *
+ *  Created on: 16 Sep 2015
+ *      Author: tyrrells
+ */
+
+#ifndef PHENOTYPE_METADATA_H_
+#define PHENOTYPE_METADATA_H_
+
+#include "pathogenomics_service_library.h"
+#include "mongodb_tool.h"
+
+#include "pathogenomics_service.h"
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* PHENOTYPE_METADATA_H_ */
---
+++ services/pathogenomics/src/phenotype_metadata.c
@@ -0,0 +1,33 @@
+/*
+ * phenotype_metadata.c
+ *
+ *  Created on: 16 Sep 2015
+ *      Author: tyrrells
+ */
+
+#include "phenotype_metadata.h"
+
+
+static char *ConvertKey (const char * const src_s);
+
+
+bool InsertPhenotypeData (MongoTool *tool_p, json_t *values_p, const char *collection_s, PathogenomicsServiceData *data_p, json_t *errors_p)
+{
+	bool success_flag = false;
+	const char *phenotype_primary_key_id_s = "Isolate";
+	const char *phenotype_primary_key_s = GetJSONString (values_p, phenotype_primary_key_id_s);
+
+	if (phenotype_primary_key_s)
+		{
+			const char *mapped_key_id_s = "UKCPVS ID";
+
+			if (SetMongoToolCollection (tool_p, data_p -> psd_samples_collection_s))
+				{
+
+				}		/* if (SetMongoToolCollection (tool_p, data_p -> psd_samples_collection_s)) */
+
+		}		/* if (phenotype_primary_key_s) */
+
+	return success_flag;
+}
+
