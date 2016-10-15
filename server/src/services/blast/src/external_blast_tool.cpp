ec9080d435dc36f6b847da95a924cb860fc8cd5d
Simon Tyrrell <tyrrell.simon@gmail.com>
2016-10-16 00:03:01 +0100

    wip

--- server/src/services/blast/src/external_blast_tool.cpp
+++ server/src/services/blast/src/external_blast_tool.cpp
@@ -266,20 +266,6 @@ char *ExternalBlastTool :: GetResults (BlastFormatter *formatter_p)
 }
 
 
-/*
-#define TAG_BLAST_INPUT_QUERY MAKE_TAG ('B', 'Q', 'U', 'Y')
-#define TAG_BLAST_INPUT_FILE MAKE_TAG ('B', 'I', 'N', 'F')
-#define TAG_BLAST_OUTPUT_FILE MAKE_TAG ('B', 'O', 'U', 'F')
-#define TAG_BLAST_MAX_SEQUENCES MAKE_TAG ('B', 'M', 'S', 'Q')
-#define TAG_BLAST_SHORT_QUERIES MAKE_TAG ('B', 'S', 'H', 'Q')
-#define TAG_BLAST_EXPECT_THRESHOLD MAKE_TAG ('B', 'E', 'X', 'T')
-#define TAG_BLAST_WORD_SIZE MAKE_TAG ('B', 'W', 'D', 'S')
-#define TAG_BLAST_MAX_RANGE_MATCHES MAKE_TAG ('B', 'M', 'R', 'G')
-#define TAG_BLAST_SUBRANGE_FROM MAKE_TAG ('B', 'Q', 'F', 'R')
-#define TAG_BLAST_SUBRANGE_TO MAKE_TAG ('B', 'Q', 'T', 'O')
-#define TAG_BLAST_MATCH_SCORE MAKE_TAG ('B', 'M', 'T', 'C')
-#define TAG_BLAST_MISMATCH_SCORE MAKE_TAG ('B', 'M', 'S', 'M')
-*/
 
 bool ExternalBlastTool :: ParseParameters (ParameterSet *params_p)
 {
@@ -292,7 +278,7 @@ bool ExternalBlastTool :: ParseParameters (ParameterSet *params_p)
 		{
 			if (AddArgsPair ("-task", value.st_string_value_s))
 				{
-					if (AddArgsPair ("-num_alignments", "5"))
+					if (AddArgsPairFromIntegerParameter (params_p, BS_NUM_ALIGNMENTS.npt_name_s, "-num_alignments", false, false))
 						{
 							if (bt_job_p -> bsj_job.sj_name_s)
 								{
